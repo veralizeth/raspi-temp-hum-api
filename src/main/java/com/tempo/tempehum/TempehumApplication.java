25ba9773c9d2f5b01ea4c104af062f1189b615d2
Vera <lizeth.veraro@outlook.com>
2020-07-13 22:38:34 -0700

    Test inserting database user and Device and print them out in Console

--- src/main/java/com/tempo/tempehum/TempehumApplication.java
+++ src/main/java/com/tempo/tempehum/TempehumApplication.java
@@ -1,13 +1,48 @@
 package com.tempo.tempehum;
 
+import com.tempo.tempehum.accessingdatapostgres.model.Device;
+import com.tempo.tempehum.accessingdatapostgres.model.User;
+import com.tempo.tempehum.accessingdatapostgres.repository.DeviceRepository;
+import com.tempo.tempehum.accessingdatapostgres.repository.UserRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Optional;
 
 @SpringBootApplication
 public class TempehumApplication {
+
+	private static final Logger log = LoggerFactory.getLogger(TempehumApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(TempehumApplication.class, args);
 	}
 
+	@Bean
+	public CommandLineRunner mappingDemo(UserRepository userRepository,
+										 DeviceRepository deviceRepository) {
+		return args -> {
+
+//			// create a new User
+//			User user = new User("John Doe", "123456");
+//
+//			// save the User
+//			userRepository.save(user);
+//
+//			// create and save new Devices
+//			deviceRepository.save(new Device("Contents", user));
+//			deviceRepository.save(new Device("Device 8", user));
+
+			Optional<User> user = userRepository.findById(4);
+
+			for (Device d : user.get().getDevices()){
+				log.info(d.getName());
+			}
+
+		};
+	}
 }
