f94e3dccde191011ef976997a8f6d2305551859f
Vera <lizeth.veraro@outlook.com>
2020-07-14 23:27:00 -0700

    Get rid off extra post annotations

--- src/main/java/com/tempo/tempehum/accessingdatapostgres/controller/DevicesController.java
+++ src/main/java/com/tempo/tempehum/accessingdatapostgres/controller/DevicesController.java
@@ -1,13 +1,10 @@
 package com.tempo.tempehum.accessingdatapostgres.controller;
 import com.tempo.tempehum.accessingdatapostgres.model.Device;
-import com.tempo.tempehum.accessingdatapostgres.model.User;
 import com.tempo.tempehum.accessingdatapostgres.repository.DeviceRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
