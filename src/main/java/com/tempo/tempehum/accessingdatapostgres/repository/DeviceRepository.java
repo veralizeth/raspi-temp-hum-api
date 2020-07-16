553249e9e246466eedd19c049b8364174fa789ab
Vera <lizeth.veraro@outlook.com>
2020-07-16 00:35:36 -0700

    Api for temperature and Humidity per deviceName

--- src/main/java/com/tempo/tempehum/accessingdatapostgres/repository/DeviceRepository.java
+++ src/main/java/com/tempo/tempehum/accessingdatapostgres/repository/DeviceRepository.java
@@ -1,9 +1,6 @@
 package com.tempo.tempehum.accessingdatapostgres.repository;
 import com.tempo.tempehum.accessingdatapostgres.model.Device;
-import com.tempo.tempehum.accessingdatapostgres.model.User;
-import org.springframework.data.domain.Sort;
 import org.springframework.data.repository.CrudRepository;
-import java.util.List;
 
 
 // This will be AUTO IMPLEMENTED by Spring into a Bean called userRepository
@@ -11,4 +8,6 @@ import java.util.List;
 
 public interface DeviceRepository extends CrudRepository<Device, Integer> {
     Device findByDeviceName(String deviceName);
+
+
 }
