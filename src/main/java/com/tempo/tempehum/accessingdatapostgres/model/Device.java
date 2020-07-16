3dc34619bcd0407c3e8336316827d18103eb8698
Vera <lizeth.veraro@outlook.com>
2020-07-15 22:29:25 -0700

    Validations for uniqueness

--- src/main/java/com/tempo/tempehum/accessingdatapostgres/model/Device.java
+++ src/main/java/com/tempo/tempehum/accessingdatapostgres/model/Device.java
@@ -11,6 +11,7 @@ public class Device implements Serializable {
     @Id
     @GeneratedValue(strategy=GenerationType.AUTO)
     private Integer id;
+    @Column(unique = true)
     private String deviceName;
 
     @ManyToOne
