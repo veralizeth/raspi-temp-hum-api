feb52e9d809401b71457d6a3df86c5d8df56a36f
Vera <lizeth.veraro@outlook.com>
2020-07-15 22:33:48 -0700

    Logic to send the SQS Messages to the database

--- src/main/java/com/tempo/tempehum/accessingdatapostgres/utils/SQSUtil.java
+++ src/main/java/com/tempo/tempehum/accessingdatapostgres/utils/SQSUtil.java
@@ -1,4 +1,5 @@
 package com.tempo.tempehum.accessingdatapostgres.utils;
+
 import com.amazonaws.auth.AWSCredentialsProvider;
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
@@ -7,13 +8,22 @@ import com.amazonaws.services.sqs.AmazonSQSClientBuilder;
 import com.amazonaws.services.sqs.model.DeleteMessageRequest;
 import com.amazonaws.services.sqs.model.Message;
 import com.amazonaws.services.sqs.model.ReceiveMessageRequest;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.tempo.tempehum.accessingdatapostgres.messages.ReportedMessage;
+import com.tempo.tempehum.accessingdatapostgres.model.Device;
+import com.tempo.tempehum.accessingdatapostgres.model.Humidity;
+import com.tempo.tempehum.accessingdatapostgres.model.Temperature;
+import com.tempo.tempehum.accessingdatapostgres.model.User;
+import com.tempo.tempehum.accessingdatapostgres.repository.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
-
 import javax.annotation.PostConstruct;
+import java.sql.Timestamp;
 import java.util.List;
+
 
 @Component
 public class SQSUtil {
@@ -31,6 +41,18 @@ public class SQSUtil {
     private String awsRegion;
 
     private AmazonSQS amazonSQS;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private DeviceRepository deviceRepository;
+
+    @Autowired
+    private TemperatureRepository temperatureRepository;
+
+    @Autowired
+    private HumidityRespository humidityRespository;
 
     private static final Logger logger = LoggerFactory.getLogger(SQSUtil.class);
 
@@ -53,18 +75,47 @@ public class SQSUtil {
                 .withWaitTimeSeconds(3);
 
         while(true) {
+            Message exceptionMessage = null;
             try {
                 final List<Message> messages = amazonSQS.receiveMessage(receiveMessageRequest).getMessages();
 
+                ObjectMapper mapper = new ObjectMapper();
+
                 for (Message messageObject : messages) {
+                    exceptionMessage = messageObject;
                     String message = messageObject.getBody();
 
-                    logger.info("Received message: " + message);
+                    ReportedMessage reportedMessage = mapper.readValue(message, ReportedMessage.class);
 
-                    deleteMessage(messageObject);
+                    System.out.println(reportedMessage.getReported().getTimestamp());
+
+                    String deviceName = reportedMessage.getReported().getDeviceName();
+                    double temperature = reportedMessage.getReported().getTemperature();
+                    double humidity = reportedMessage.getReported().getHumidity();
+                    Timestamp timestamp = reportedMessage.getReported().getTimestamp();
+
+                    Device device = deviceRepository.findByDeviceName(deviceName);
+                    User user = UserRepositoryHelper.getDefaultUser(userRepository);
+
+                    if (device == null){
+                        device = new Device(deviceName, user);
+                        deviceRepository.save(device);
+                    }else {
+
+                        Temperature temperatureReported = new Temperature(temperature, device, timestamp);
+                        temperatureRepository.save(temperatureReported);
+
+                        Humidity humidityReported = new Humidity(humidity, device, timestamp);
+                        humidityRespository.save(humidityReported);
+
+                        deleteMessage(messageObject);
+                    }
+//                  deleteMessage(messageObject);
                 }
             }catch (Exception e) {
-                break;
+                if (exceptionMessage != null) {
+                    deleteMessage(exceptionMessage);
+                }
             }
         }
     }
