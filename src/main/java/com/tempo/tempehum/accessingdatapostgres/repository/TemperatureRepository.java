4f43108a12beee69b4a17aa7020479c637da63f1
Vera <lizeth.veraro@outlook.com>
2020-07-16 00:37:00 -0700

    Temperature list per deviceName

--- src/main/java/com/tempo/tempehum/accessingdatapostgres/repository/TemperatureRepository.java
+++ src/main/java/com/tempo/tempehum/accessingdatapostgres/repository/TemperatureRepository.java
@@ -1,10 +1,13 @@
 package com.tempo.tempehum.accessingdatapostgres.repository;
-
+import com.tempo.tempehum.accessingdatapostgres.model.Device;
 import com.tempo.tempehum.accessingdatapostgres.model.Temperature;
 import org.springframework.data.repository.CrudRepository;
-
+import java.util.List;
 
 
 public interface TemperatureRepository extends CrudRepository<Temperature, Integer> {
-
+//
+//    @Query("SELECT * FROM temperatures join devices on fk_device = devices.id")
+//    public Iterable<Temperature> findByDevice(@Param("deviceName") String deviceName);
+    List <Temperature> findByDevice(Device device);
 }
