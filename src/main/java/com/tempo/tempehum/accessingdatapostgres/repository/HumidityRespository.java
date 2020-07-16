5ddaf4d5399abbd47bf0a662b3773df311cec0f1
Vera <lizeth.veraro@outlook.com>
2020-07-16 00:36:06 -0700

    Return List of Humidity per device

--- src/main/java/com/tempo/tempehum/accessingdatapostgres/repository/HumidityRespository.java
+++ src/main/java/com/tempo/tempehum/accessingdatapostgres/repository/HumidityRespository.java
@@ -1,8 +1,12 @@
 package com.tempo.tempehum.accessingdatapostgres.repository;
+import com.tempo.tempehum.accessingdatapostgres.model.Device;
 import com.tempo.tempehum.accessingdatapostgres.model.Humidity;
+import com.tempo.tempehum.accessingdatapostgres.model.Temperature;
 import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
 
 
 public interface HumidityRespository extends CrudRepository<Humidity, Integer> {
-
+    List<Humidity> findByDevice(Device device);
 }
